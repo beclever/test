12b6df634df48889b20a2e676a459ece6194f1f5
beclever <849191840@qq.com>
2015-09-22 13:17:06 +0800

    修改部分注释

--- src/main/java/com/showshine/interceptor/AuthorityInterceptor.java
+++ src/main/java/com/showshine/interceptor/AuthorityInterceptor.java
@@ -7,6 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+/**
+ * 
+ * 版权所有：2015-GRGBANKING
+ * 项目名称：camara   
+ *
+ * 类描述：
+ * 类名称：com.showshine.interceptor.AuthorityInterceptor     
+ * 创建人：WSH
+ * 创建时间：2015年9月22日 下午1:16:11   
+ * 修改人：
+ * 修改时间：2015年9月22日 下午1:16:11   
+ * 修改备注：   
+ * @version   V1.0
+ */
 public class AuthorityInterceptor extends HandlerInterceptorAdapter  {
 
     Logger logger = LoggerFactory.getLogger(getClass());
